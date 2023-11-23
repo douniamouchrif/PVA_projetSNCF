e8e901a82fef214f0e4964651c97923f6c967713
mathildetissandier <mathilde.tissandier@gmail.com>
2023-11-23 13:10:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-import dash
+from dash import Dash, html
 
-app = dash()
-app.start
+app = Dash(__name__)
+
+app.layout = html.Div([
+
+], id='page-content')
+
+if __name__ == '__main__':
+    app.run_server()
