ef51932744db324acac5986f176e338fa5f28f42
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2023-11-24 10:49:24 +0100

    Update scatterplot.py

--- visus/scatterplot.py
+++ visus/scatterplot.py
@@ -1,8 +1,7 @@
-import data.datas1
+import pandas as pd
 import plotly.express as px
-from itertools import groupby
-from datetime import datetime
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
 db = client['projetSNCF']
 
@@ -12,22 +11,18 @@ def get_data():
         'gravite_epsf': {'$ne': None},
         'date': {'$gte': '2023-01-01', '$lte': '2023-08-31'}
     }, {'gravite_epsf': 1, 'date': 1})
-    data = list(result)
 
-    grouped_data = {k: {'gravités': [x['gravite_epsf'] for x in g]}
-                    for k, g in groupby(sorted(data, key=lambda x: x['date'][:7]), key=lambda x: x['date'][:7])}
-    for month, values in grouped_data.items():
-        values['gravite_moyenne'] = sum(
-            values['gravités']) / len(values['gravités'])
+    df = pd.DataFrame(result)
+    df['date'] = pd.to_datetime(df['date'])
+    df['Mois'] = df['date'].dt.to_period('M')
+    grouped_data = df.groupby('Mois')['gravite_epsf'].mean().reset_index()
+
     return grouped_data
 
 
-data = get_data()
-df = []
-for month, values in data.items():
-    df.append({'Mois': datetime.strptime(month, '%Y-%m'),
-               'Gravité moyenne': values['gravite_moyenne']})
+df = get_data()
+df['Mois'] = df['Mois'].dt.strftime('%Y-%m')
 
-fig = px.scatter(df, x='Mois', y='Gravité moyenne',
-                 title='Gravité moyenne par mois', color='Gravité moyenne', size='Gravité moyenne')
+fig = px.scatter(df, x='Mois', y='gravite_epsf',
+                 title='Gravité moyenne par mois', color='gravite_epsf', size='gravite_epsf')
 fig.show()
