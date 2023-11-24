5a9e8fbcb4a415afb4bba3127433117c64534b33
romank2 <102798439+k-roman5@users.noreply.github.com>
2023-11-24 10:48:55 +0100

    Ajout des commentaires
    Ajout des commentaires pour les deux barplot

--- visus/barplot_2015-2022.py
+++ visus/barplot_2015-2022.py
@@ -14,27 +14,28 @@ collection = db['sncf1522']
 cursor = collection.find({}, {'region': 1, 'origine': 1, 'niveau_gravite': 1, 'date': 1})
 df = pd.DataFrame(list(cursor))
 
+# Extraction de l'année à partir de la colonne de date
 df['year'] = pd.to_datetime(df['date']).dt.year
 
-# Convert 'niveau_gravite' column to numeric
+# Conversion de la colonne 'niveau_gravite' en numérique
 df['niveau_gravite'] = pd.to_numeric(df['niveau_gravite'], errors='coerce')
 
-# Get the top 5 regions and incident types by the most incidents
+# Les 5 principales régions et types d'incidents avec le plus d'incidents
 top_regions = df['region'].value_counts().nlargest(5).index
 top_types = df['origine'].value_counts().nlargest(5).index
 
-# Create a RangeSlider for selecting years
+# Création d'un curseur interactif (range slider) pour sélectionner une plage d'années
 years_range_slider = IntRangeSlider(
     value=[df['year'].min(), df['year'].max()],
     min=df['year'].min(),
     max=df['year'].max(),
     step=1,
-    description='Years:',
+    description='Années:',
     continuous_update=False,
     orientation='horizontal',
 )
 
-# Function to update the plot based on the selected years
+# Fonction de mise à jour du graphique en fonction de la plage d'années sélectionnée
 def update_plot(years):
     selected_data = df[(df['year'] >= years[0]) & (df['year'] <= years[1])]
     top_data = selected_data[selected_data['region'].isin(top_regions) & selected_data['origine'].isin(top_types)]
@@ -43,19 +44,18 @@ def update_plot(years):
     mean_gravity_df = mean_gravity_df[mean_gravity_df.sum().sort_values(ascending=False).index]
     mean_gravity_df = mean_gravity_df.loc[mean_gravity_df.sum(axis=1).sort_values(ascending=False).index]
 
-    # Clear the previous output
+    # Effacer la sortie précédente
     clear_output(wait=True)
 
-    # Create a new figure and axis
+    # Créer et afficher le nouveau graphique
     fig, ax = plt.subplots(figsize=(12, 8))
     mean_gravity_df.plot(kind='bar', ax=ax)
-    ax.set_xlabel('Region')
-    ax.set_ylabel('Mean Gravity')
-    ax.set_title('Mean Gravity of Top 5 Incident Types in Top 5 Regions')
-    ax.legend(title='Incident Type', bbox_to_anchor=(1, 1), loc='upper left')
+    ax.set_xlabel('Région')
+    ax.set_ylabel('Gravité Moyenne')
+    ax.set_title('Gravité Moyenne des 5 Principaux Types d\'Incidents dans les 5 Principales Régions')
+    ax.legend(title='Type d\'Incident', bbox_to_anchor=(1, 1), loc='upper left')
 
-# Create an interactive widget
+# Creation du graphique interactif
 interactive_plot = interactive(update_plot, years=years_range_slider)
 
-# Display the interactive widget
 display(interactive_plot)
--- visus/barplot_2023.py
+++ visus/barplot_2023.py
@@ -12,28 +12,27 @@ collection = db['sncf23']
 cursor = collection.find({}, {'region': 1, 'origine': 1, 'gravite_epsf': 1})
 df = pd.DataFrame(list(cursor))
 
-# Get the top 5 regions and incident types by the most incidents
+# Les 5 principales régions et types d'incidents avec le plus d'incidents
 top_regions = df['region'].value_counts().nlargest(5).index
 top_types = df['origine'].value_counts().nlargest(5).index
 
-# Filter data for the top regions and types
+# Les données pour les principales régions et types
 top_data = df[df['region'].isin(top_regions) & df['origine'].isin(top_types)]
 
-# Create a new dataframe with mean gravity for each region and incident type
+# Nouveau dataframe avec la gravité moyenne pour chaque région et type d'incident
 mean_gravity_df = top_data.groupby(['region', 'origine'])['gravite_epsf'].mean().unstack()
 
-# Sort data by the most incidents for both regions and incident types
+# Trier les données par le nombre d'incidents pour les régions et les types d'incidents
 mean_gravity_df = mean_gravity_df[mean_gravity_df.sum().sort_values(ascending=False).index]
 mean_gravity_df = mean_gravity_df.loc[mean_gravity_df.sum(axis=1).sort_values(ascending=False).index]
 
-# Plotting using Seaborn for better color handling
+# Gestion des couleurs
 sns.set_palette("husl")
 ax = mean_gravity_df.plot(kind='bar', figsize=(12, 8))
-ax.set_xlabel('Region')
-ax.set_ylabel('Mean Gravity')
-ax.set_title('Mean Gravity of Top 5 Incident Types in Top 5 Regions')
+ax.set_xlabel('Région')
+ax.set_ylabel('Gravité Moyenne')
+ax.set_title('Gravité Moyenne des 5 Principaux Types d\'Incidents dans les 5 Principales Régions')
 
-# Display legend outside the plot
-ax.legend(title='Incident Type', bbox_to_anchor=(1, 1), loc='upper left')
+ax.legend(title='Type d\'Incident', bbox_to_anchor=(1, 1), loc='upper left')
 
 plt.show()
