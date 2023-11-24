a9076eb6f42d351bade1124de8324de67406869b
mathildetissandier <mathilde.tissandier@gmail.com>
2023-11-24 10:26:00 +0100

    Update bam.py

--- visus/bam.py
+++ visus/bam.py
@@ -7,20 +7,25 @@ import pandas as pd
 client = MongoClient('localhost', 27017)
 db = client['projetSNCF']
 collection = db["sncf1522"]
+
 # Récupération des données depuis MongoDB
 cursor = collection.find({})
 data = list(cursor)
+
 # Prétraitement des données
 df = pd.DataFrame(data)
 df['date'] = pd.to_datetime(df['date'], errors='coerce')  # Conversion en datetime
 df['year'] = df['date'].dt.strftime('%Y')
+
 # Filtrage des lignes avec des valeurs NaN dans les colonnes 'year' et 'origine'
 df_filtered = df.dropna(subset=['year', 'origine'])
+
 # Création du graphique en boîtes avec Plotly Express et menu déroulant
 fig = px.box(df_filtered, x='year', y=df_filtered.groupby('year').cumcount(), labels={'y': 'Nombre d incidents'},
              title='Nombre d incidents par année',
              category_orders={'year': sorted(df_filtered['year'].unique())},
              animation_group='origine')
+
 # Ajout de la ligne verticale pour l'année 2019
 fig.add_trace(go.Scatter(
     x=['2019', '2019'],
@@ -30,6 +35,7 @@ fig.add_trace(go.Scatter(
     showlegend=True,  # Afficher la légende pour cette trace
     name='Début de la crise du COVID-19'
 ))
+
 # Mise à jour du layout
 fig.update_layout(
     title_font=dict(size=24),
@@ -38,7 +44,7 @@ fig.update_layout(
         tickfont=dict(size=18),
     ),
     yaxis=dict(title=dict(text='Nombre d incidents', font=dict(size=22)),
-               tickfont=dict(size=18),),
+               tickfont=dict(size=18)),
     legend=dict(
         x=1.02,
         y=0.5,
@@ -47,5 +53,6 @@ fig.update_layout(
         bgcolor='rgba(255, 255, 255, 0.5)',
     )
 )
+
 # Affichage du graphique dans un navigateur web
 fig.show()
