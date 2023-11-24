a9793f7a0a759854b858fbc274e0ec0cfe8a0a61
douniamouchrif <102798610+douniamouchrif@users.noreply.github.com>
2023-11-24 11:32:50 +0100

    Create database.py

---
+++ data/database.py
@@ -0,0 +1,46 @@
+from pymongo import MongoClient
+import requests
+
+url_SNCF = 'https://data.sncf.com/api/explore/v2.1/catalog/datasets/'
+
+
+def fetch_and_insert_data(db, collection_name, url, limit=100):
+    collection = db[collection_name]
+    offset = 0
+
+    while True:
+        payload = {'limit': limit, 'offset': offset}
+        response = requests.get(url, params=payload)
+
+        if response.status_code == 200:
+            data = response.json()
+            collection.insert_many(data['results'])
+            if len(data['results']) < limit:
+                break
+            else:
+                offset += limit
+
+
+# Provide the connection details
+hostname = 'localhost'
+port = 27017  # Default MongoDB port
+
+# Create a MongoClient instance
+client = MongoClient(hostname, port)
+
+db = client['projetSNCF']  # projetSNCF = database de notre projet
+
+url23 = url_SNCF + 'incidents-de-securite-epsf/records'
+fetch_and_insert_data(db, "sncf23", url23)
+
+url1522 = url_SNCF + 'incidents-securite/records'
+fetch_and_insert_data(db, "sncf1522", url1522)
+
+urlLigneE = url_SNCF + 'liste-des-lignes-electrifiees/records'
+fetch_and_insert_data(db, "sncfLigneE", urlLigneE)
+
+url_l_admin = url_SNCF + 'lignes-par-region-administrative/records'
+fetch_and_insert_data(db, "sncf_l_admin", url_l_admin)
+
+urlLigneT = url_SNCF + 'lignes-par-type/records'
+fetch_and_insert_data(db, "sncfLigneT", urlLigneT)
