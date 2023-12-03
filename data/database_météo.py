93708488af726ca5d740cef9df3943b42e038e4f
mathildetissandier <mathilde.tissandier@gmail.com>
2023-12-03 09:44:40 +0100

    récupérer 9.9k données météo

--- data/database_météo.py
+++ data/database_météo.py
@@ -19,38 +19,29 @@ def fetch_and_insert_data(db, collection_name, url, limit=100):
 
         if response.status_code == 200:
             data = response.json()
+            print(data)
 
-            # Filter data for the period between 2015 and 2023
-            filtered_data = []
-            for entry in data['results']:
-                date = entry.get('date')
-                if date >= '2015-01-01' and date <= '2023-12-31':
-                    filtered_data.append({
-                        'date': entry.get('date'),
-                        'non_reg': entry.get('non_reg'),
-                        'ff': entry.get('ff'),
-                        'tc': entry.get('tc'),
-                        'rr24': entry.get('rr24'),
-                        'coordonnees': entry.get('coordonnees'),
-                    })
+            # Select only the specified columns
+            selected_data = [
+                {
+                    'date': entry['date'],
+                    'non_reg': entry['nom_reg'],
+                    'ff': entry['ff'],
+                    'tc': entry['tc'],
+                    'rr24': entry['rr24'],
+                    'coordonnees': entry['coordonnees'],
+                }
+                for entry in data['results']
+            ]
 
-        print(f"Number of entries before filtering: {len(data['results'])}")
-        print(f"Number of entries after filtering: {len(filtered_data)}")
+            collection.insert_many(selected_data)
 
-        if len(filtered_data) == 0:
-            print("No data meets the filtering conditions.")
-            return
-
-        collection.insert_many(filtered_data)
-
-        if len(data['results']) < limit:
-            return
-        else:
-            fetch_data_and_insert(offset + limit)
+            if len(data['results']) < limit:
+                return
+            else:
+                fetch_data_and_insert(offset + limit)
 
     fetch_data_and_insert(offset)
-
-
 
 url_météo = 'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/donnees-synop-essentielles-omm/records'
 fetch_and_insert_data(db, "météo", url_météo)
