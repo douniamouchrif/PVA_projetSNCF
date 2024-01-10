819c484611d0068f515af51c7367ee85ab4bb23c
mathildetissandier <102798509+mathildetissandier@users.noreply.github.com>
2024-01-10 21:29:46 +0100

    story telling pour le boxplot

--- pages/vis1.py
+++ pages/vis1.py
@@ -3,26 +3,27 @@ from dash import html, dcc, callback, Input, Output
 import dash_bootstrap_components as dbc
 from visus.visualisation import build_boxplot
 from data.get_data import get_data_boxplot_t
+from story_telling.box_plot_story import get_text_below_boxplot
 
 question = "Le nombre d’incidents varie-t-il drastiquement d’une année à l’autre?"
 
 dash.register_page(__name__, question=question, external_stylesheets=[
     dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
-
 def layout():
     boxplot_content = build_boxplot(get_data_boxplot_t())
     figure_size = {'width': '100%', 'height': '750px'}
     graph = dcc.Graph(figure=boxplot_content, style=figure_size)
+    
     return [html.Div(children=[
         html.Div([
             dbc.Button("Retour", id="btn-retour1", color="primary",
                        className="mr-1", style={'float': 'right', 'background-color': '#670907'}),
             html.H3(question, style={'textAlign': 'center'}),
         ]),
-        html.Div(graph)
+        html.Div(graph),
+        get_text_below_boxplot()
     ]), dcc.Location(id='url-redirect1')]
-
 
 @callback(
     Output("url-redirect1", "pathname"),
---
+++ story_telling/box_plot_story.py
@@ -0,0 +1,31 @@
+from dash import html
+
+def get_text_below_boxplot():
+    return html.Div([
+        "Box plot montrant la répartition du nombre d'accidents ferroviaires par an de 2015 à 2023.",
+        html.Br(),
+        "Vous trouverez ci-dessous une analyse détaillée de chaque année :",
+        html.Br(),
+        "2015 : L’année avec le plus grand nombre d’incidents. La médiane est supérieure à 150 événements et il existe des valeurs extrêmes supérieures à 200 événements.",
+        html.Br(),
+        "2016 : Une légère diminution par rapport à 2015, avec une médiane d'environ 120 accidents. La dispersion des données est similaire à celle de 2015.",
+        html.Br(),
+        "2017 : La médiane et la dispersion des événements ont continué à diminuer légèrement par rapport à 2016.",
+        html.Br(),
+        "2018 : La médiane est similaire à celle de 2017, et la dispersion aussi avec des valeurs extrêmes inférieures.",
+        html.Br(),
+        "2019 : Marqué par la ligne rouge pointillée, qui indique le début de la crise du COVID-19, nous observons une diminution significative du nombre médian d’événements, envrion 81 événements, avec une moindre dispersion que les années précédentes.",
+        html.Br(),
+        "2020 : Le nombre d’événements a légérement diminué depuis 2019. La médiane est d'environ 91 événements, et la dispersion a quelque peu augmentée.",
+        html.Br(),
+        "2021 : Une légère diminution par rapport à 2020, avec une médiane de 78 accidents. La dispersion reste plus faible par rapport aux années antérieures à 2020.",
+        html.Br(),
+        "2022 : Le nombre d’événements a diminué et minimal, avec une médiane proche de 2019 (69 événements) et une dispersion similaire à 2021.",
+        html.Br(),
+        "2023 : L’année en cours ou la dernière année enregistrée présente une médiane plus élevée que 2022, avec une dispersion importante et des valeurs extrêmes assez élevées.",
+        html.Br(),
+        html.U("En résumé, il existe une différence significative dans le nombre d’accidents ferroviaires par an entre 2015 et 2023. Le changement le plus important s’est produit entre 2019 et 2020, correspondant au début de la crise du COVID-19, lorsque le nombre d’accidents a considérablement diminué."),
+        html.Br(),
+        html.U("À mesure que les trains circulent moins fréquemment, les risques d’accidents diminueront naturellement, ce qui pourrait entraîner une diminution de la fréquence des accidents signalés. Par ailleurs, les opérations de maintenance et d'inspection des voies ferrées et du matériel roulant auraient pu être réalisées de manière plus régulière ou avec plus d'attention pendant cette période, car des horaires moins chargés auraient permis un accès plus facile aux infrastructures sans interruption du service."),
+        html.U("Comme le montrent les données, à mesure que les restrictions liées au COVID-19 seront levées et que la vie commencera à revenir à la normale, le nombre de trains en circulation pourrait commencer à augmenter, entraînant une augmentation progressive du nombre d'accidents. L’augmentation plus importante en 2023 suggère un retour plus complet du trafic ferroviaire, ou éventuellement d’autres facteurs contribuant à l’augmentation des accidents."),
+    ], style={'margin-top': '20px', 'font-size': '16px'})
