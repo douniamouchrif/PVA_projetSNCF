840a1c84a66d569cd8783ae6aea928ad791a6054
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2024-01-11 13:30:17 +0100

    correction

--- pages/home.py
+++ pages/home.py
@@ -32,21 +32,21 @@ layout = html.Div(style={'color': 'black', 'min-height': '100vh'}, children=[
         html.H4(
             'Nous vous souhaitons à toutes et à tous une agréable expérience. Attention au départ !',
             style={'textAlign': 'center', 'fontSize': '1.75em'}),
-        
+
         html.Img(src='/static/IMG/gare.png', style={'width': '20%', 'display': 'block',
                  'margin-left': 'auto', 'margin-right': 'auto', 'margin-bottom': '20px'}),
-        
-        html.P("Tout d’abord, pourquoi ces données ? Nous les avons choisies car nous cherchions un thème en rapport avec les déplacements et la sécurité routière en France,"
-                "en particulier les incidents de la route, d’avion, ... puis nous avons trouvé une base de données sur les incidents de la SNCF sur laquelle nous nous sommes intéressées. " 
-                "Par la suite, sur ce même site nous avons trouvé d'autres bases de données sur les lignes que nous avons décidé de récupérer pour notre projet. "
-                "À travers ce dashboard, nous allons vous présenter les questions que nous nous sommes posées sur ces données et les visualisations que nous avons réalisé pour y répondre.", style={'fontSize': '1.25em'}),
-        
+
+        html.P("Tout d’abord, pourquoi ces données ? Nous les avons choisies car nous cherchions un thème en rapport avec les déplacements et la sécurité routière en France, "
+               "en particulier les incidents de la route, d’avion, ... puis nous avons trouvé une base de données sur les incidents de la SNCF sur laquelle nous nous sommes intéressées. "
+               "Par la suite, sur ce même site nous avons trouvé d'autres bases de données sur les lignes que nous avons décidé de récupérer pour notre projet. "
+               "À travers ce dashboard, nous allons vous présenter les questions que nous nous sommes posées sur ces données et les visualisations que nous avons réalisé pour y répondre.", style={'fontSize': '1.25em'}),
+
         html.P("Pour la récupération des données, nous avons mis en place une utilisation de l'outil MongoDB "
-                "avec l'API afin d'assurer une mise à jour régulière et une précision maximale. Les données sont extraites "
-                "à partir du site officiel de la SNCF et sont traitées de manière à fournir des informations "
-                "pertinentes et à jour.", style={'fontSize': '1.25em'}),
+               "avec l'API afin d'assurer une mise à jour régulière et une précision maximale. Les données sont extraites "
+               "à partir du site officiel de la SNCF et sont traitées de manière à fournir des informations "
+               "pertinentes et à jour.", style={'fontSize': '1.25em'}),
         html.P("Les 5 bases de données que nous utilisons sont les suivantes :", style={
-                'fontSize': '1.25em'}),
+            'fontSize': '1.25em'}),
         html.Div(style={'display': 'flex', 'flexDirection': 'column', 'fontSize': '1.25em'}, children=[
             html.Li(html.A("Incidents de sécurité (Evénements de sécurité remarquables - ESR) de janvier 2015 à décembre 2022",
                     href='https://data.sncf.com/explore/dataset/incidents-securite/table/?sort=date', target='_blank')),
@@ -59,22 +59,22 @@ layout = html.Div(style={'color': 'black', 'min-height': '100vh'}, children=[
                     href='https://data.sncf.com/explore/dataset/lignes-par-region-administrative/table/', target='_blank')),
             html.Li(html.A("Météo",
                     href='https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/table/?sort=date&dataChart=eyJxdWVyaWVzIjpbeyJjaGFydHMiOlt7InR5cGUiOiJjb2x1bW4iLCJmdW5jIjoiQVZHIiwieUF4aXMiOiJ0YyIsInNjaWVudGlmaWNEaXNwbGF5Ijp0cnVlLCJjb2xvciI6IiNGRjUxNUEifV0sInhBeGlzIjoiZGF0ZSIsIm1heHBvaW50cyI6IiIsInRpbWVzY2FsZSI6ImRheSIsInNvcnQiOiIiLCJjb25maWciOnsiZGF0YXNldCI6ImRvbm5lZXMtc3lub3AtZXNzZW50aWVsbGVzLW9tbSIsIm9wdGlvbnMiOnt9fX1dLCJkaXNwbGF5TGVnZW5kIjp0cnVlLCJhbGlnbk1vbnRoIjp0cnVlfQ%3D%3D', target='_blank'))
-        ]), 
+        ]),
         html.Br(),
-        html.P("Nos deux premières bases de données concernant les incidents de 2015 à 2022 et de 2023, continnent des informations sur les incidents : "
-                "la date, l'origine, le type d'événement, la région et le lieu et ainsi que le niveau de gravité.", style={'fontSize': '1.25em'}),
-        html.P("Chaque incident est classé selon l'échelle de gravité ci-dessous (de 1 à 6). Comme c'est indiqué, cette échelle à été mise en place en 2016, "
-                "cela explique que dans nos bases de données les incidents avant 2016 n'ont pas de gravité associée.", style={'fontSize': '1.25em'}),
-        
+        html.P("Nos deux premières bases de données concernant les incidents de 2015 à 2022 et de 2023, contiennent des informations sur les incidents : "
+               "la date, l'origine, le type d'évènements, la région et le lieu, ainsi que le niveau de gravité.", style={'fontSize': '1.25em'}),
+        html.P("Chaque incident est classé selon l'échelle de gravité ci-dessous (de 1 la plus faible, à 6 la plus forte). Comme indiqué, cette échelle a été mise en place en 2016, "
+               "cela explique que dans nos bases de données les incidents avant 2016 n'ont pas de gravité associée.", style={'fontSize': '1.25em'}),
+
         html.Img(src='/static/IMG/gravite.png', style={'width': '40%', 'display': 'block',
                  'margin-left': 'auto', 'margin-right': 'auto', 'margin-bottom': '20px'}),
         html.P("Image issue du site : https://www.cheminots.net/topic/42925-prise-en-écharpe/", style={'width': '30%', 'display': 'block',
-                 'margin-left': 'auto', 'margin-right': 'auto', 'margin-bottom': '20px', 'fontSize': '0.15em'}),
-        
-        html.P("Dans nos 3 bases de données concernant les lignes, nous allons nous intérésser aux colonnes des coordonnées géographiques et des régions, dans le but de construire des cartes pour les visualiser."
-                , style={'fontSize': '1.25em'}),
-        
-        html.P("Enfin dans la base de données de la météo, parmis les 82 colonnes, nous allons nous intéresser à la température, la date, la région, les précipitations, et les coordonnées géographiques."
-                , style={'fontSize': '1.25em'}),
+                                                                                                       'margin-left': 'auto', 'margin-right': 'auto', 'margin-bottom': '20px', 'fontSize': '0.15em'}),
+
+        html.P("Dans nos 3 bases de données concernant les lignes, nous allons nous intéresser aux colonnes des coordonnées géographiques et des régions, dans le but de construire des cartes pour les visualiser.", style={
+               'fontSize': '1.25em'}),
+
+        html.P("Enfin, dans la base de données de la météo, parmis les 82 colonnes, nous allons nous intéresser à la température, la date, la région, les précipitations, et les coordonnées géographiques.", style={
+               'fontSize': '1.25em'}),
     ])
 ])
